041935e6dd5359bd8a010188f69081697ec2197f
Swann Pincet <swann.pincet@gmail.com>
2024-05-04 19:48:07 +0200

    overrides generated object

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
 import { _generate } from "./helpers/generator";
 
-export const generate = (targetName: string): Record<any, any> => _generate(targetName)
+export const generate = (targetName: string, overrides: Record<string, any> = {}): Record<any, any> => {
+    const generated = _generate(targetName)
 
-console.log(generate('ComplexObjectInterface'))
+    if (!Array.isArray(generated) && typeof generated === "object") {
+        for (const evaluated in overrides) {
+            eval(`generated.${evaluated} = ${JSON.stringify(overrides[evaluated])}`)
+        }
+    }
+
+    return generated
+}
+
+console.log(generate('ComplexObjectInterface', {
+    'nested.nestedInterfaces': [{ ho: 'ho' }],
+    'nested.nestedType': { ha: 'ha' },
+    'random': 'he',
+    'id': 69
+}))
